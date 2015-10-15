c5be79a91363e4cf9c3bbe27c54716d4330aca08
mayfender <mayfender@mayfender-PC>
2015-10-15 21:55:34 +0700

    update

--- parking-center/src/main/java/com/may/ple/parking/center/constant/RolesConstant.java
+++ parking-center/src/main/java/com/may/ple/parking/center/constant/RolesConstant.java
@@ -1,8 +1,9 @@
 package com.may.ple.parking.center.constant;
 
 public enum RolesConstant {
-	ROLE_GATEWAY(1, "Gateway"), 
-	ROLE_ADMIN(2, "Admin");
+	ROLE_GATEWAY_IN(1, "Gateway In"), 
+	ROLE_GATEWAY_OUT(2, "Gateway Out"), 
+	ROLE_ADMIN(3, "Admin");
 	
 	private int id;
 	private String name;
