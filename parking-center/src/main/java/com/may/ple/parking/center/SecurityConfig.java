824884f493defb1a4c6d2d3a136a5ab3da7329f4
mayfender <mayfender@mayfender-PC>
2015-10-24 18:34:06 +0700

    update

--- parking-center/src/main/java/com/may/ple/parking/center/SecurityConfig.java
+++ parking-center/src/main/java/com/may/ple/parking/center/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement().maximumSessions(1);
 		http.httpBasic()
 		.and()
 		.csrf().disable()
